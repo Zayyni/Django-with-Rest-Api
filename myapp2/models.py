607edbf95dd8613206d7a6f293aa16cf82069f7f
zayyni <zainulislam.733@gmail.com>
2025-04-24 23:22:03 +0500

    implemented models

--- myapp2/models.py
+++ myapp2/models.py
@@ -7,3 +7,18 @@ class Employee(models.Model):
     mail = models.EmailField(max_length=30)
     age = models.IntegerField()
 
+
+    # def __str__(self):
+    #     return self.name
+
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=30)
+    mail = models.EmailField(max_length=30)
+    age = models.IntegerField()
+
+
+    class Meta:
+            db_table = 'student_details'
